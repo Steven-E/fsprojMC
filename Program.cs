1732cd7d4042d9f93c57265214e8a427c3111518
Steven Etienne <steven.a.etienne@gmail.com>
2014-06-23 21:33:57 -0500

    Querying OS for file/folder info
    Got rid of RegEx(bad idea anyways..) Now querying MS OS for file/folder
    type.
    
    Currently works assuming the program has permissions/rights to access
    folder or file type.
    
    Spits out a bunch of garbage that does nothing but prove to me/you that
    I can recursively get folders and files from a specified path.
    
    Next step is to push it all to a sql DB.
    
    Next is to pull information from a DB and populate itself.
    
    Next is to use IMDB API to pull object information etc.
    
    After that create UI for populating information/content (File
    name/actors/runtime/poster etc).
    
    After that we'll see...

--- Program.cs
+++ Program.cs
@@ -3,43 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Text.RegularExpressions;
+/**
+ * 
+ * Note!!! Have not sorted out file permissions yet. 
+ * So if you attempt to pass a path in which this program does not have 
+ * permissions or access to it will break (i.e. Recycle Bin folders)...
+ * still. it sorta works. 
+ * 
+ * */
 
 namespace recursive_sep_file_folder
 {
     class Program
     {
-        static void recSepFF(string origPath) 
+        static void recSepFF(string origPath)
         {
-            string[] fileNfolders = Directory.GetFileSystemEntries(origPath);
-            //alright, so getting rid of the RegEx bad idea :(
-            //string fileExp = @"\.+\w+\w?\w?\w$";
+            LinkedList<string> ffList = new LinkedList<string>();
 
-            //string folderExp = @"\\[A-Z0-9]$";
+            int folderNum = 0;
+            int fileNum = 0;
 
-            string[] files = new string[50];
-
-            string[] folders = new string[50];
-
-            int fileSize = 0;
-            int folderSize = 0;
-
-            foreach (string s in fileNfolders)
+            foreach (string entry in Directory.GetDirectories(origPath))
             {
-                if (!Directory.Exists(s))
-                {
-                    files[fileSize] = s.Substring(s.LastIndexOf('\\') + 1);
-                    fileSize++;
-                }
-                else if (Directory.Exists(s))
-                {
-             
-                    folders[folderSize] = s;
-                    folderSize++;
-                
-                }
-
-                //if the above is not true, then i have no worries. continue. 
+                ffList.AddLast(entry);
+                folderNum++;
+            }
+            foreach (string entry in Directory.GetFiles(origPath))
+            {
+                ffList.AddLast(entry);
+                fileNum++;
             }
 
             Console.WriteLine("====================================================");
@@ -47,45 +39,78 @@ namespace recursive_sep_file_folder
             Console.WriteLine("====================================================");
 
             //Printing results out to screen;
-            Console.WriteLine("The number of folders we have is " + folderSize + ". \nThe folders we recorded are: \n");
+            Console.WriteLine("The number of folders we have is " + folderNum + ". \nThe folders we recorded are: \n");
 
-            for (int i = 0; i < folderSize; i++)
-                Console.WriteLine("\t" + folders[i]);
+            displayFolders(ffList);
 
-            Console.WriteLine("\n\nThe number of files we have is " + fileSize + ".\nThe files we recorded are: \n");
+            Console.WriteLine("\n\nThe number of files we have is " + fileNum + ".\nThe files we recorded are: \n");
 
-            for (int i = 0; i < fileSize; i++)
-                Console.WriteLine("\t" + files[i]);
+            displayFiles(ffList);
 
             Console.WriteLine("");
 
-
-            if (folderSize >= 1)
+            if (folderNum >= 1)
             {
-                for (int i = 0; i < folderSize; i++)
-                    recSepFF(folders[i]);
-            }            
+                foreach (string item in ffList)
+                {
+                    if (Directory.Exists(item))
+                    {
+                        recSepFF(item);
+                    }
+                }
+            }
         }
 
-        static string getPathFromUser() 
+        static string pathVerif()
         {
-            string userPath;
-            Console.WriteLine("Please specify the path you would like to use:\n>");
+            string path = @"NULL";
 
-            userPath = Console.ReadLine();
-
-            while (!Directory.Exists(userPath)) 
+            do
             {
-                Console.WriteLine("Sorry...That directory does not exits.\nPlease enter another VALID directory:\n>");
-                userPath = Console.ReadLine();
-            }
+                Console.WriteLine("Please specify path to use:>");
+                path = Console.ReadLine();
 
-            return userPath;
+                if (Directory.Exists(path))
+                    Console.WriteLine("Path \"" + path + "\" exists! Continueing on..");
+                else
+                    Console.WriteLine("That path is not valid/exists.\nPlease try again:\n");
+
+            } while (!Directory.Exists(path));
+
+            return path;
+        }
+
+
+        static void displayFolders(LinkedList<string> inList)
+        {
+            Console.WriteLine("Folders recorded: \n>>");
+            foreach (string item in inList)
+            {
+                if (Directory.Exists(item))
+                {
+                    Console.Write(">" + item);
+                    Console.WriteLine("Is the directory valid? " + Directory.Exists(item));
+                }
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        static void displayFiles(LinkedList<string> inList)
+        {
+            Console.WriteLine("Files recorded: \n>>");
+            foreach (string item in inList)
+            {
+                if (File.Exists(item))
+                {
+                    Console.Write(">" + item + "\n");
+                    Console.WriteLine("is the file valid? " + File.Exists(item));
+                }
+            }
         }
 
         static void Main(string[] args)
         {
-            string somePath = getPathFromUser();
+            string somePath = pathVerif();
 
             recSepFF(somePath);
         }
